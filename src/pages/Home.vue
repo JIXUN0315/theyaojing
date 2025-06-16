27f885b28b4e5aca11db44e0e0fc52eec74dd8d2
Tom <tomtang@richitech.com.tw>
2025-06-16 20:34:33 +0800

    initial commit

---
+++ src/pages/Home.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1>Home Page</h1>
+    <router-link to="/about">Go to About</router-link>
+    <router-link to="/template">Go to template</router-link>
+  </div>
+</template>
---
+++ src/pages/Template.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="template">
+    <h1>{{ text }}</h1>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const text = ref('模板')
+</script>
+
+<style lang="scss" scoped>
+.template {
+  h1 {
+    color: #42b983;
+    font-weight: bold;
+    padding: 1rem;
+    &:hover {
+      color: #036137;
+    }
+  }
+}
+</style>
